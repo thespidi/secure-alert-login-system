83c2086c5cf725c84765697f5f1179db97c867d4
Ateeque Belal <atikbilal2000@gmail.com>
2026-06-02 13:17:51 +0530

    Move email credentials to environment variables

--- mailer.js
+++ mailer.js
@@ -1,25 +1,31 @@
+require('dotenv').config();
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'atikbilal2000@gmail.com',
-    pass: 'ucsspbaetzybwvpc'
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   }
 });
 
 function sendAlert(to, message) {
   const mailOptions = {
-    from: '"Secure Login System" <atikbilal2000@gmail.com>',
+    from: `"Secure Login System" <${process.env.MAIL_USER}>`,
     to: to,
     subject: 'Security Alert 🚨',
     text: message
   };
 
   transporter.sendMail(mailOptions, (err, info) => {
-    if (err) console.log(err);
-    else console.log("Email sent");
+    if (err) {
+      console.log("Mail Error:", err);
+    } else {
+      console.log("Email sent to:", to);
+    }
   });
 }
+
+module.exports = sendAlert;
 
 module.exports = sendAlert;
