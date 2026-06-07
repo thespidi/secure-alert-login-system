c6fd717d2821c9270cd1d5b7936e4db0fec9bd3f
Ateeque Belal <atikbilal2000@gmail.com>
2026-06-07 14:55:54 +0530

    Prepare project for Railway deployment

--- db.js
+++ db.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.MYSQLHOST || process.env.DB_HOST,
+  user: process.env.MYSQLUSER || process.env.DB_USER,
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
+  port: process.env.MYSQLPORT || 3306
 });
 
 db.connect((err) => {
